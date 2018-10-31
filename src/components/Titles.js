9d42484f26073f406279baf3826e17f3504f1123
Zoe ZHANG <chris521zyy@gmail.com>
2018-10-31 19:08:26 +1000

    first update the weather app, React.js application

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1 className="title-container__title">Weather Report</h1>
+                <p className="title-container__subtitle">Find out temperautre, conditions, and more...</p>
+            </div>
+            );
+
+        }
+    };
+
+
+export default Titles;
